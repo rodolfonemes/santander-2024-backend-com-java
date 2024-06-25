91d7df679257934e1554b369a23d1f0bbabcb265
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2024-06-25 16:31:18 -0300

    Initial commit

---
+++ src/test/java/com/rodolfonemes/Santander2024BackendComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodolfonemes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2024BackendComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
